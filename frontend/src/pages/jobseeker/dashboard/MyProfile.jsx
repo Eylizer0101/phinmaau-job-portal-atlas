5d42c029682d55dd637d7ba1cbbf83b1d093b17b
Eylizer Marcos <eylizermarcos@gmail.com>
2026-06-08 12:30:31 +0800

    Fix apply job modal again

--- frontend/src/pages/jobseeker/dashboard/MyProfile.jsx
+++ frontend/src/pages/jobseeker/dashboard/MyProfile.jsx
@@ -1104,6 +1104,7 @@ const BasicInfoModal = ({
                 >
                   {profileImageUploading ? <Spinner size="small" /> : <FaCamera className="text-xs" />}
                 </button>
+
               </div>
             </div>
 
