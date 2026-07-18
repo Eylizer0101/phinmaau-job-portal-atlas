3c2e73ea6a5790c6870799b60c27b2e4acf60cfb
Eylizer Marcos <eylizermarcos@gmail.com>
2026-07-18 23:12:42 +0800

    fix apply job modal

--- backend/routes/communityRoutes.js
+++ backend/routes/communityRoutes.js
@@ -5,16 +5,31 @@ const { uploadCommunityImage } = require('../middleware/uploadMiddleware');
 const {
   getPosts,
   createPost,
+  updatePost,
+  deletePost,
   toggleLike,
   getComments,
   addComment,
+  reactToComment,
+  addReply,
+  reportContent,
+  getManagedContent,
 } = require('../controllers/communityController');
 
 router.use(protect);
+
 router.get('/posts', getPosts);
 router.post('/posts', uploadCommunityImage.single('image'), createPost);
+router.put('/posts/:postId', uploadCommunityImage.single('image'), updatePost);
+router.delete('/posts/:postId', deletePost);
+
 router.post('/posts/:postId/like', toggleLike);
 router.get('/posts/:postId/comments', getComments);
 router.post('/posts/:postId/comments', addComment);
+router.post('/posts/:postId/comments/:commentId/reaction', reactToComment);
+router.post('/posts/:postId/comments/:commentId/replies', addReply);
+
+router.post('/reports', reportContent);
+router.get('/managed', getManagedContent);
 
 module.exports = router;
