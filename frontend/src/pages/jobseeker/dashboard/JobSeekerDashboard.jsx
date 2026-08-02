ac3b9d2b0c92fee41cae8dce622ceb78416b8da5
Eylizer Marcos <eylizermarcos@gmail.com>
2026-08-02 21:58:49 +0800

    Fix apply job modal again

--- frontend/src/pages/jobseeker/dashboard/JobSeekerDashboard.jsx
+++ frontend/src/pages/jobseeker/dashboard/JobSeekerDashboard.jsx
@@ -1304,6 +1304,16 @@ const JobSeekerDashboard = () => {
                     autoComplete="current-password"
                   />
                   <button
+                          type="button"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleViewCompanyJobs(c);
+                          }}
+                          className="whitespace-nowrap border-0 bg-transparent p-0 text-[13px] font-semibold text-[#2e66a6] shadow-none transition hover:text-[#245387]"
+                        >
+                          {jobCount} New Job Offer{jobCount === 1 ? '' : 's'}
+                        </button>
+                  <button
                     type="button"
                     onClick={() => togglePasswordVisibility('currentPassword')}
                     className="absolute inset-y-0 right-0 inline-flex items-center px-4 text-gray-500"
@@ -2328,17 +2338,6 @@ const JobSeekerDashboard = () => {
                           >
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
                           </svg>
-                        </button>
-
-                        <button
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleViewCompanyJobs(c);
-                          }}
-                          className="whitespace-nowrap border-0 bg-transparent p-0 text-[13px] font-semibold text-[#2e66a6] shadow-none transition hover:text-[#245387]"
-                        >
-                          {jobCount} New Job Offer{jobCount === 1 ? '' : 's'}
                         </button>
                       </div>
                     </div>
