3c9932179775f8d20f6fd04fb83ed2c34d12a0fa
Eylizer Marcos <eylizermarcos@gmail.com>
2026-08-25 10:29:20 +0800

    Fix apply job modal again

--- frontend/src/pages/employer/dashboard/Settings.jsx
+++ frontend/src/pages/employer/dashboard/Settings.jsx
@@ -452,7 +452,7 @@ const Settings = () => {
   return (
     <EmployerLayout>
       <div className="min-h-screen  px-4 py-8 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-6xl">
+        <div className="mx-auto max-w-7xl px-1 py-8">
           <button
             type="button"
             onClick={() => navigate(-1)}
