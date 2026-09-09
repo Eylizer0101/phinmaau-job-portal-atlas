61c39fdf2a4c1165064b313463a8a01228b6b22e
Eylizer Marcos <eylizermarcos@gmail.com>
2026-09-09 23:18:56 +0800

    Fix apply job modal again

--- frontend/src/pages/admin/UserManagementDetails.jsx
+++ frontend/src/pages/admin/UserManagementDetails.jsx
@@ -17,8 +17,8 @@ const JobCardIcon = ({ name, className = "w-4 h-4" }) => {
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth="1.8"
-          d="M8 7V5a2 2 0 012-2h4a2 2 0 012 2v2m3 0H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2z"
+          strokeWidth="2"
+          d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
         />
       </svg>
     );
