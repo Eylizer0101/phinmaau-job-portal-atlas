c3779a606fd7cb6ecc2cbabd72105a3795be70c5
Eylizer Marcos <eylizermarcos@gmail.com>
2026-09-12 16:37:37 +0800

    Fix apply job modal again

--- frontend/src/pages/employer/dashboard/ManageJobs.jsx
+++ frontend/src/pages/employer/dashboard/ManageJobs.jsx
@@ -1684,7 +1684,7 @@ const ManageJobs = () => {
                   })}
                 </div>
 
-                <div className="hidden max-h-[660px] overflow-x-auto overflow-y-auto overscroll-auto md:block">
+                <div className="hidden max-h-[508px] overflow-x-auto overflow-y-auto overscroll-auto md:block">
                 <div className="">
                   <table className="min-w-full divide-y divide-gray-200">
                     <colgroup>
